8e1c137da48c68fb74a7bc0a9df6b600c8dc8811
donkeypagoda <christopherallenstewart@gmail.com>
2017-12-21 14:37:29 -0800

    getting reqs together

---
+++ jsonObjExample.js
@@ -0,0 +1,122 @@
+{
+  "business":{
+    "business_name":"Tuley Law Office",
+    "logo_image": {
+      "type": "logo",
+      "external_url": "http://demo.cloudfrontend.net/img/upload/6157627_sized.jpg"
+    },
+    "partner_category_id": "2800",
+    "partner_business_id":null,
+    "partner_sub_id":null,
+    "locations":[
+      {
+        "automatic_latlon":true,
+        "borough":null,
+        "city":"Evansville",
+        "contact_email":null,
+        "country":"US",
+        "description":"<p>Here is the business description</p>",
+        "facebook_url":"https://www.facebook.com/pages/Tuley-Law-Office/659534027457013?ref=br_tf",
+        "gplus_url":null,
+        "homepage_url":null,
+        "hours_of_operation":[
+          [
+            [
+              "08:00:00",
+              "16:30:00"
+            ],
+            [
+
+            ]
+          ],
+          [
+            [
+              "08:00:00",
+              "16:30:00"
+            ],
+            [
+
+            ]
+          ],
+          [
+            [
+              "08:00:00",
+              "16:30:00"
+            ],
+            [
+
+            ]
+          ],
+          [
+            [
+              "08:00:00",
+              "16:30:00"
+            ],
+            [
+
+            ]
+          ],
+          [
+            [
+              "08:00:00",
+              "16:00:00"
+            ],
+            [
+
+            ]
+          ],
+          [
+            [
+
+            ],
+            [
+
+            ]
+          ],
+          [
+            [
+
+            ],
+            [
+
+            ]
+          ]
+        ],
+        "hours_of_operation_notes":null,
+        "instagram_url":null,
+        "linkedin_url":null,
+        "neighborhood":null,
+        "partner_location_id":null,
+        "payment_forms":[
+
+        ],
+        "payment_forms_notes":null,
+        "phones":[
+          {
+            "extension":null,
+            "number":"(812) 434-1936",
+            "type":"phone"
+          }
+        ],
+        "postal_code":"47708",
+        "private_street":null,
+        "service_area_only":false,
+        "service_areas":null,
+        "state":"IN",
+        "street":"20 NW 1st Street Suite 610",
+        "tagline":"Fighting For You! Indiana's Personal Injury Attorneys",
+        "twitter_url":null,
+        "year_opened":null,
+        "yelp_url":null,
+        "youtube_url":null
+      }
+    ]
+  },
+  "formatted_domain":"tuleylaw.demosites2.com",
+  "noindex":false,
+  "partner_site_id":null,
+  "partner_sub_id":null,
+  "published":true,
+  "title":"Tuley Law Office",
+  "type":"default"
+}
---
+++ projectInfo.md
@@ -0,0 +1,30 @@
+https://www.geekwire.com/2017/drop-tune-seattle-mobile-analytics-companys-founders-set-tech-outpost-alaska/
+ 
+Thanks for stopping by today. Here are the details for the project.
+ 
+Setup a simple form to collect info. Form can have default values set to help with testing
+•           Business Name
+•           Street
+•           City
+•           State (i.e. WA)
+•           Postal Code
+•           Country (i.e. US)
+•           Phone Number
+Submit will serialize the forms fields, and store them in a JSON object (here is an example of a full json with all fields https://gist.github.com/dlrust/f50c0c6b1c1e416ccd75)
+ 
+Will launch a modal window. Modal should allow for closing of the modal, changing values, and then re-launching the modal
+ 
+The modal will contain:
+ 
+1) iframe. Which will be a URL generated that passes the string version of the JSON to render a preview Site. JSON string is passed as the "site" url param. clone_id will be static.
+ 
+Example URL: http://cloudtemplates.cloudfrontend.net/app/live-preview/?clone_id=1576931&site={%22business%22:{%22business_name%22:%20%22Johnson%20Legal%20Partners%22}}
+ 
+2) The modal window should have two buttons
+•           Close - Closes the modal window to allow the user to edit their business fields again
+•           Export - Will console.log the raw JSON object (not the "stringified" version)
+---
+ 
+You can use any tech, framework, or plugins you feel comfortable with. If you could create a github repo (public is fine) for the code.
+ 
+Thanks and let me know if you have any questions.
