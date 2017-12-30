2cf2f285cb1c43cb7a02e9a45d41ffeeeb7e5b3e
donkeypagoda <christopherallenstewart@gmail.com>
2017-12-30 14:12:57 -0800

    fixed the modal to force user into using buttons only, back to fixing iframe

--- form.js
+++ form.js
@@ -1,25 +1,5 @@
-let jsonObj = {
-  // "business_name": "",
-  // "street": "",
-  // "city": "",
-  // "postal_code": "",
-  // "country": "",
-  // "phone_number": ""
-}
+let jsonObj = {}
 let siteURL = ""
-
-// hard coded option
-// const bizForm = document.getElementById("bizForm");
-// const formBiz = document.getElementById("formBiz");
-// const formStreet = document.getElementById("formStreet");
-// const formCity = document.getElementById("formCity");
-// const formState = document.getElementById("formState");
-// const formPc = document.getElementById("formPc");
-// const formCountry = document.getElementById("formCountry");
-// const formPhone = document.getElementById("formPhone");
-// const formButton = document.getElementById("formButton");
-
-// iterative option
 const bizForm = document.getElementById("bizForm");
 const formButton = document.getElementById("formButton");
 
@@ -37,44 +17,19 @@ formButton.onclick = (e) => {
 const modal = new tingle.modal({
     footer: true,
     stickyFooter: false,
-    closeMethods: ['overlay', 'button', 'escape'],
+    closeMethods: ['escape'],
     closeLabel: "Close",
     cssClass: ['custom-class-1', 'custom-class-2'],
-    onOpen: function() {
-        console.log('modal open');
-    },
-    onClose: function() {
-        console.log('modal closed');
-    },
-    // beforeClose: function() {
-    //     // here's goes some logic
-    //     // e.g. save content before closing the modal
-    //     return true; // close the modal
-    // 	return false; // nothing happens
-    // }
 });
 
-// set content
 modal.setContent('<iframe>http://cloudtemplates.cloudfrontend.net/app/live-preview/?clone_id=1576931&site={siteURL}</iframe>');
 
-// add a button
-modal.addFooterBtn('Button label', 'tingle-btn tingle-btn--primary', function() {
-    // here goes some logic
+modal.addFooterBtn('Close', 'tingle-btn tingle-btn--primary', function() {
     modal.close();
 });
 
-// add another button
-modal.addFooterBtn('Dangerous action !', 'tingle-btn tingle-btn--danger', function() {
-    // here goes some logic
+modal.addFooterBtn('Export', 'tingle-btn tingle-btn--danger', function() {
+    console.log(jsonObj);
+    bizForm.reset();
     modal.close();
 });
-
-// open modal
-// modal.open();
-
-// close modal
-// modal.close();
-
-// function formSubmit(){
-//   modal.open();
-// }
