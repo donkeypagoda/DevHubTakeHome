5b6e99f7203a0c5b103412a7cf19ac316a436441
donkeypagoda <christopherallenstewart@gmail.com>
2017-12-30 16:07:26 -0800

    cleaning up and fixing string literal

--- form.js
+++ form.js
@@ -3,6 +3,24 @@ let siteURL = ""
 const bizForm = document.getElementById("bizForm");
 const formButton = document.getElementById("formButton");
 
+const modal = new tingle.modal({
+  footer: true,
+  stickyFooter: false,
+  closeMethods: ['escape'],
+  closeLabel: "Close",
+  cssClass: ['custom-class-1', 'custom-class-2'],
+});
+
+modal.addFooterBtn('Close', 'tingle-btn tingle-btn--primary', function() {
+  modal.close();
+});
+
+modal.addFooterBtn('Export', 'tingle-btn tingle-btn--danger', function() {
+  console.log(jsonObj);
+  bizForm.reset();
+  modal.close();
+});
+
 formButton.onclick = (e) => {
   e.preventDefault();
 
@@ -10,30 +28,9 @@ formButton.onclick = (e) => {
     let key = bizForm.elements[i].id;
     jsonObj[key] = bizForm.elements[i].value;
   }
-
+  
   siteURL = JSON.stringify(jsonObj);
   const url= `http://cloudtemplates.cloudfrontend.net/app/live-preview/?clone_id=1576931&site=${encodeURIComponent(siteURL)}`
   modal.setContent(`<iframe src=${url}></iframe>`);
-
   modal.open();
 };
-
-const modal = new tingle.modal({
-    footer: true,
-    stickyFooter: false,
-    closeMethods: ['escape'],
-    closeLabel: "Close",
-    cssClass: ['custom-class-1', 'custom-class-2'],
-});
-
-modal.setContent('<iframe src="http://cloudtemplates.cloudfrontend.net/app/live-preview/?clone_id=1576931&site={siteURL}"></iframe>');
-
-modal.addFooterBtn('Close', 'tingle-btn tingle-btn--primary', function() {
-    modal.close();
-});
-
-modal.addFooterBtn('Export', 'tingle-btn tingle-btn--danger', function() {
-    console.log(jsonObj);
-    bizForm.reset();
-    modal.close();
-});
